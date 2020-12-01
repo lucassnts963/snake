cd1b41a19175a1e1fefbefa6ec602ef327077bda
Lucas Santos <lucassnts963@gmail.com>
2020-12-01 20:33:17 -0300

    Add some comments

--- food.py
+++ food.py
@@ -6,13 +6,15 @@ from config import Configuration
 
 config = Configuration()
 
+#Object that represent the food
 class Food:
     def __init__(self, game):
         x = rd.randint(0,config.screen_size[0]/config.size_rect-1) * config.size_rect
         y = rd.randint(0,config.screen_size[0]/config.size_rect-1) * config.size_rect
         self.position = (x,y)
         self.game = game
-        
+    
+    #Method to draw the food at the screen    
     def draw(self):
         apple = pygame.Surface((config.size_rect, config.size_rect))
         apple.fill(config.colors['red'])
--- snake.py
+++ snake.py
@@ -9,6 +9,7 @@ RIGHT = 4
 
 config = Configuration()
 
+#Object that represent the snake of the game
 class Snake:
     
     def __init__(self, game):
@@ -18,12 +19,14 @@ class Snake:
         self.direction = RIGHT
         self.score = 0
     
+    #Method to draw the snake at the screen
     def draw(self):
         skin = pygame.Surface((config.size_rect, config.size_rect))
         skin.fill(config.colors['white'])
         for pos in self.body:
             self.game.screen.blit(skin,pos)
     
+    #Method to update of the position the snake at the screen
     def update(self):
         step = config.size_rect
         if self.direction == UP:
@@ -47,21 +50,25 @@ class Snake:
         self.eat(self.game.food)
         self.draw()
     
+    #Method to restart the game
     def restart(self):
         self.body = [(0,0),(10,0),(20,0)]
         self.direction = RIGHT
         self.game.food.__init__(self.game)
         self.score = 0
     
+    #Method to change the direction of the snake
     def change_direction(self, direction):
         self.direction = direction
     
+    #Method to check if the snake eat the food
     def eat(self, food):
         if self.body[0] == food.position:
             self.body.append((food.position))
             self.game.food.__init__(self.game)
             self.score += 1
-            
+
+    #Method to check if was a collision
     def get_collision(self):    
         if self.body[0][0] >= config.screen_size[0]:
             self.restart()
@@ -75,9 +82,3 @@ class Snake:
             if self.body[0][0] == self.body[i][0] and self.body[0][1] == self.body[i][1]:
                 self.restart()
                 break
-            
-    def get_collision_tail(self, pos):
-        for i in range(1, len(self.body)-1):
-            print(len(self.body)-1)
-            if pos == self.body[i]:
-                self.restart()
